0f23395602a53e2c33aec7fd47d21da837d6db7e
Dirk Kretschmann <kretschmanndi@gmail.com>
2025-11-29 10:43:04 +0100

    add blinking led

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wokwi-rust-demo
+
+```sh
+cargo build --release
+elf2uf2-rs target/thumbv6m-none-eabi/release/wokwi-test target/thumbv6m-none-eabi/release/wokwi-test.uf2
+```
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::digital::v2::OutputPin;
+use panic_halt as _;
+use rp_pico::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // LED setup
+    let mut led = pins.led.into_push_pull_output();
+    
+    // Blink forever
+    loop {
+        led.set_high().unwrap();
+        delay.delay_ms(500);
+        led.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
